470284f6e300554373cff3085ca75711be7e6d4f
Daphne Balane <balanedaphne20@gmail.com>
2023-08-27 19:08:49 +0800

    test: returned and fixed initial test cases

--- src/test/java/com/afs/restapi/CompanyApiTest.java
+++ src/test/java/com/afs/restapi/CompanyApiTest.java
@@ -20,8 +20,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -41,65 +44,56 @@ class CompanyApiTest {
 
     @Test
     void should_find_companies() throws Exception {
-        CompanyRequest companyRequest = new CompanyRequest("OOCL");
-        Company company = companyRepository.save(new Company(null, companyRequest.getName()));
+        Company company = companyRepository.save(getCompanyOOCL());
 
         mockMvc.perform(get("/companies"))
-                .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(company.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(company.getName()));
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].id").value(company.getId()))
+                .andExpect(jsonPath("$[0].name").value(company.getName()))
+                .andExpect(jsonPath("$[0].employeesCount").exists());
     }
 
     @Test
     void should_find_company_by_id() throws Exception {
-        CompanyRequest companyRequest = new CompanyRequest("OOCL");
-        Company company = new Company(null, companyRequest.getName());
-        Company createdCompany = companyRepository.save(company);
-
-        EmployeeRequest employeeRequest = new EmployeeRequest("Alice", 23, "Female", 5000, createdCompany.getId());
-        Employee createdEmployee = employeeRepository.save(new Employee(null,
-                employeeRequest.getName(),
-                employeeRequest.getAge(),
-                employeeRequest.getGender(),
-                employeeRequest.getSalary(),
-                createdCompany.getId()));
+        Company company = companyRepository.save(getCompanyOOCL());
+        Employee employee = employeeRepository.save(getEmployee(company));
 
         mockMvc.perform(get("/companies/{id}", company.getId()))
-                .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(createdCompany.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(createdCompany.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees.length()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].id").value(createdEmployee.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].name").value(createdEmployee.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].age").value(createdEmployee.getAge()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].gender").value(createdEmployee.getGender()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[0].salary").value(createdEmployee.getSalary()));
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.id").value(company.getId()))
+                .andExpect(jsonPath("$.name").value(company.getName()))
+                .andExpect(jsonPath("$.employeesCount").value(1))
+                .andExpect(jsonPath("$.employees[0].id").value(1L))
+                .andExpect(jsonPath("$.employees[0].name").value(employee.getName()))
+                .andExpect(jsonPath("$.employees[0].age").value(employee.getAge()))
+                .andExpect(jsonPath("$.employees[0].gender").value(employee.getGender()))
+                .andExpect(jsonPath("$.employees[0].salary").doesNotExist());
     }
 
     @Test
     void should_update_company_name() throws Exception {
         Company previousCompany = companyRepository.save(new Company(null, "Facebook"));
-        CompanyRequest companyRequest = new CompanyRequest("Thoughtworks");
-
+        CompanyRequest updatedCompanyRequest = new CompanyRequest("Meta");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String updatedCompanyRequestJSON = objectMapper.writeValueAsString(updatedCompanyRequest);
         mockMvc.perform(put("/companies/{id}", previousCompany.getId())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(companyRequest)))
-                .andExpect(MockMvcResultMatchers.status().is(204));
+                        .content(updatedCompanyRequestJSON))
+                .andExpect(status().is(204));
 
         Optional<Company> optionalCompany = companyRepository.findById(previousCompany.getId());
         assertTrue(optionalCompany.isPresent());
         Company updatedCompany = optionalCompany.get();
-        Assertions.assertEquals(previousCompany.getId(), updatedCompany.getId());
-        Assertions.assertEquals(companyRequest.getName(), updatedCompany.getName());
+        assertEquals(previousCompany.getId(), updatedCompany.getId());
+        assertEquals(updatedCompanyRequest.getName(), updatedCompany.getName());
     }
 
     @Test
     void should_delete_company_name() throws Exception {
-        CompanyRequest companyRequest = new CompanyRequest("OOCL");
-        Company company = companyRepository.save(new Company(null, companyRequest.getName()));
+        Company company = companyRepository.save(getCompanyGoogle());
         mockMvc.perform(delete("/companies/{id}", company.getId()))
-                .andExpect(MockMvcResultMatchers.status().is(204));
+                .andExpect(status().is(204));
 
         assertTrue(companyRepository.findById(company.getId()).isEmpty());
     }
@@ -113,52 +107,72 @@ class CompanyApiTest {
         mockMvc.perform(post("/companies")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(companyRequestJSON))
-                .andExpect(MockMvcResultMatchers.status().is(201))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(notNullValue()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(companyRequest.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employeesCount").exists());
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.id").value(notNullValue()))
+                .andExpect(jsonPath("$.name").value(companyRequest.getName()))
+                .andExpect(jsonPath("$.employeesCount").exists());
     }
 
     @Test
     void should_find_companies_by_page() throws Exception {
-        CompanyRequest companyRequest1 = new CompanyRequest("OOCL");
-        Company firstCompany = companyRepository.save(new Company(null, companyRequest1.getName()));
-
-        CompanyRequest companyRequest2 = new CompanyRequest("Thoughtworks");
-        Company secondCompany = companyRepository.save(new Company(null, companyRequest2.getName()));
+        Company oocl = companyRepository.save(getCompanyOOCL());
+        Company thoughtworks = companyRepository.save(getCompanyThoughtWorks());
+        companyRepository.save(getCompanyGoogle());
 
         mockMvc.perform(get("/companies")
                         .param("pageNumber", "1")
                         .param("pageSize", "2"))
-                .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(firstCompany.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(firstCompany.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(secondCompany.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(secondCompany.getName()));
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(oocl.getId()))
+                .andExpect(jsonPath("$[0].name").value(oocl.getName()))
+                .andExpect(jsonPath("$[0].employeesCount").exists())
+                .andExpect(jsonPath("$[1].id").value(thoughtworks.getId()))
+                .andExpect(jsonPath("$[1].name").value(thoughtworks.getName()))
+                .andExpect(jsonPath("$[1].employeesCount").exists());
     }
 
     @Test
     void should_find_employees_by_companies() throws Exception {
-        CompanyRequest companyRequest = new CompanyRequest("OOCL");
-        Company createdCompany = companyRepository.save(new Company(null, companyRequest.getName()));
+        Company oocl = companyRepository.save(getCompanyOOCL());
+        Employee employee = employeeRepository.save(getEmployee(oocl));
 
-        EmployeeRequest employeeRequest = new EmployeeRequest("Alice", 18, "Female", 2000, createdCompany.getId());
-        Employee createdEmployee = employeeRepository.save(new Employee(null,
-                employeeRequest.getName(),
-                employeeRequest.getAge(),
-                employeeRequest.getGender(),
-                employeeRequest.getSalary(),
-                createdCompany.getId()));
-        employeeRepository.save(createdEmployee);
+        mockMvc.perform(get("/companies/{companyId}/employees", oocl.getId()))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].id").value(employee.getId()))
+                .andExpect(jsonPath("$[0].name").value(employee.getName()))
+                .andExpect(jsonPath("$[0].age").value(employee.getAge()))
+                .andExpect(jsonPath("$[0].gender").value(employee.getGender()))
+                .andExpect(jsonPath("$[0].salary").doesNotExist());
+    }
 
-        mockMvc.perform(get("/companies/{companyId}/employees", createdCompany.getId()))
-                .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(createdEmployee.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(createdEmployee.getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(createdEmployee.getAge()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value(createdEmployee.getGender()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(createdEmployee.getSalary()));
+    private static Employee getEmployee(Company company) {
+        Employee employee = new Employee();
+        employee.setName("Bob");
+        employee.setAge(22);
+        employee.setGender("Male");
+        employee.setSalary(10000);
+        employee.setCompanyId(company.getId());
+        return employee;
+    }
+
+
+    private static Company getCompanyOOCL() {
+        Company company = new Company();
+        company.setName("OOCL");
+        return company;
+    }
+
+    private static Company getCompanyThoughtWorks() {
+        Company company = new Company();
+        company.setName("Thoughtworks");
+        return company;
+    }
+
+    private static Company getCompanyGoogle() {
+        Company company = new Company();
+        company.setName("Google");
+        return company;
     }
 }
