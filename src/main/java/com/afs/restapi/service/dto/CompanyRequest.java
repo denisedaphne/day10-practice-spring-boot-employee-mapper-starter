d10a5be29532b213895a62027f0810d606281ff8
Daphne Balane <balanedaphne20@gmail.com>
2023-08-27 19:10:40 +0800

    refactor: removed unused methods

--- src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
+++ src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
@@ -3,18 +3,14 @@ package com.afs.restapi.service.dto;
 public class CompanyRequest {
     private String name;
 
-    public CompanyRequest(){
-
+    public CompanyRequest() {
     }
-    public CompanyRequest(String name){
+
+    public CompanyRequest(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public String setName() {
         return name;
     }
 }
